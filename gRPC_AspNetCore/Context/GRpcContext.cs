ae5a2d49071afe29c9e41077608e8d5b5ee329ee
Masoud Kian <masoudkiannezhad@gmail.com>
2025-05-17 20:50:53 +0330

    Add project files.

---
+++ gRPC_AspNetCore/Context/GRpcContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gRPC_AspNetCore.Context
+{
+    public class GRpcContext : DbContext
+    {
+        public GRpcContext(DbContextOptions<GRpcContext> options) : base(options)
+        {
+            
+        }
+    }
+}
