f8d89c79c5f6c3428ac4f1ea4b21b874396f9405
Masoud Kian <masoudkiannezhad@gmail.com>
2025-05-18 08:00:18 +0330

    set Reflection

--- gRPC_AspNetCore/Program.cs
+++ gRPC_AspNetCore/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddDbContext<GRpcContext>(options =>
 
 #endregion
 
+#region Reflection gRPC
+
+builder.Services.AddGrpcReflection();
+
+#endregion
+
 // Add services to the container.
 builder.Services.AddGrpc();
 
@@ -21,6 +27,15 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<ProductService>();
+
+#region Continue for Reflection
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
+#endregion
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
